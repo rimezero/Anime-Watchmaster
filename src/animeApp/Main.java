9647bea583918e1a1ff45bdb1802be9aa3e78e32
Rime <Rime@DESKTOP-S3UQKDJ>
2019-02-28 01:51:00 +0200

    test

--- src/animeApp/Main.java
+++ src/animeApp/Main.java
@@ -12,6 +12,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
     static Scene mainscene, allanimescene;
     static Stage window;
+    
 
     @Override
     public void start(Stage primaryStage) throws Exception{
